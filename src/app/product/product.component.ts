b44a783bfa459f94819a66f95cc593aba778ad98
Patrice-H <webmestre64@gmail.com>
2023-02-16 20:01:40 +0100

    Define product component class

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  styleUrls: ['./product.component.scss'],
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
+  id!: number;
+  title!: string;
+  price!: number;
+  description!: string;
+  category!: string;
+  image!: string;
 
+  ngOnInit() {}
 }
