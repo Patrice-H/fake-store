7e34e85ee90c51e0985b381842df1cc7a55fe186
Patrice-H <webmestre64@gmail.com>
2023-02-16 20:21:39 +0100

    Create the product component structure

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -13,5 +13,13 @@ export class ProductComponent implements OnInit {
   category!: string;
   image!: string;
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.id = 1;
+    this.title = 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops';
+    this.price = 109.95;
+    this.description =
+      'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday';
+    this.category = "men's clothing";
+    this.image = 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg';
+  }
 }
