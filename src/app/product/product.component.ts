689028bf7caf2d99703b15bad2862d6b0023581a
Patrice-H <webmestre64@gmail.com>
2023-02-18 00:03:19 +0100

    Add decorator to simplify the component

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,25 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from '../models/product.model';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss'],
 })
-export class ProductComponent implements OnInit {
-  id!: number;
-  title!: string;
-  price!: number;
-  description!: string;
-  category!: string;
-  image!: string;
-
-  ngOnInit() {
-    this.id = 1;
-    this.title = 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops';
-    this.price = 109.95;
-    this.description =
-      'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday';
-    this.category = "men's clothing";
-    this.image = 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg';
-  }
+export class ProductComponent {
+  @Input() product!: Product;
 }
