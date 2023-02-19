05fba3c05636c83e17a9b9c31c784f8b6e127f6d
Patrice-H <webmestre64@gmail.com>
2023-02-20 00:25:39 +0100

    Add dynamic rubric

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Product } from '../models/product.model';
 
 @Component({
@@ -6,6 +6,36 @@ import { Product } from '../models/product.model';
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss'],
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
   @Input() product!: Product;
+  currentRubric!: string;
+
+  ngOnInit() {
+    this.currentRubric = this.getRubric(this.product.category);
+  }
+
+  getRubric(category: string): string {
+    switch (category) {
+      case 'smartphones' || 'laptops':
+        return 'high-tech';
+      case 'home-decoration' || 'lighting' || 'furniture':
+        return 'home';
+      case 'fragances' || 'skincare':
+        return 'cosmetic';
+      case 'automotive' || 'motorcycle':
+        return 'auto-moto';
+      case 'mens-shirts' || 'mens-shoes':
+        return 'man';
+      case 'tops' || 'womens-dresses' || 'womens-shoes':
+        return 'woman';
+      case 'mens-watches' ||
+        'womens-watches' ||
+        'womens-bags' ||
+        'womens-jewellery' ||
+        'sunglasses':
+        return 'accessory';
+      default:
+        return 'other';
+    }
+  }
 }
