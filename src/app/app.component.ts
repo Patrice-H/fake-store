ece5f8d049f69b5b6f50044f58641160379c7c2e
Patrice-H <webmestre64@gmail.com>
2023-02-23 11:14:39 +0100

    Simplify app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,23 +6,7 @@ import { Product } from './models/product.model';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  product!: Product;
+
   ngOnInit(): void {
-    this.product = new Product(
-      1,
-      'iPhone 9',
-      'An apple mobile which is nothing like apple',
-      549,
-      12.96,
-      'smartphones',
-      'https://i.dummyjson.com/data/products/1/thumbnail.jpg',
-      [
-        'https://i.dummyjson.com/data/products/1/1.jpg',
-        'https://i.dummyjson.com/data/products/1/2.jpg',
-        'https://i.dummyjson.com/data/products/1/3.jpg',
-        'https://i.dummyjson.com/data/products/1/4.jpg',
-        'https://i.dummyjson.com/data/products/1/thumbnail.jpg',
-      ]
-    );
   }
 }
