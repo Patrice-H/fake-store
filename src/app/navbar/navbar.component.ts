5a5e5a8fc12d9e580626be7b22e3c264573597c8
Patrice-H <webmestre64@gmail.com>
2023-02-24 12:25:19 +0100

    Add click event on menu items

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,16 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class NavbarComponent {
-constructor() {}
+  constructor(private router: Router) {}
+
+  showProducts(category: string): void {
+    this.router.navigate([`/${category}`]);
+  }
 }
