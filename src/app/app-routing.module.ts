6f5435946d91efc3ee09da6b1cd2476d9909debe
Patrice-H <webmestre64@gmail.com>
2023-02-24 12:23:55 +0100

    Add category routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,29 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsListComponent } from './products-list/products-list.component';
 
-const routes: Routes = [{ path: '', component: ProductsListComponent }];
+const routes: Routes = [
+  { path: '', component: ProductsListComponent },
+  { path: 'smartphones', component: ProductsListComponent },
+  { path: 'laptops', component: ProductsListComponent },
+  { path: 'home-decoration', component: ProductsListComponent },
+  { path: 'lighting', component: ProductsListComponent },
+  { path: 'furniture', component: ProductsListComponent },
+  { path: 'fragances', component: ProductsListComponent },
+  { path: 'skincare', component: ProductsListComponent },
+  { path: 'automotive', component: ProductsListComponent },
+  { path: 'motorcycle', component: ProductsListComponent },
+  { path: 'tops', component: ProductsListComponent },
+  { path: 'womens-dresses', component: ProductsListComponent },
+  { path: 'womens-shoes', component: ProductsListComponent },
+  { path: 'mens-shirts', component: ProductsListComponent },
+  { path: 'mens-shoes', component: ProductsListComponent },
+  { path: 'mens-watches', component: ProductsListComponent },
+  { path: 'womens-watches', component: ProductsListComponent },
+  { path: 'womens-bags', component: ProductsListComponent },
+  { path: 'womens-jewellery', component: ProductsListComponent },
+  { path: 'sunglasses', component: ProductsListComponent },
+  { path: 'groceries', component: ProductsListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
