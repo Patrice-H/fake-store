391f96382cd309cc7de0330b1b29521c2552f9c2
Patrice-H <webmestre64@gmail.com>
2023-02-26 00:07:45 +0100

    Add a route to 404 error page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404Component } from './error404/error404.component';
 import { ProductComponent } from './product/product.component';
 import { ProductsListComponent } from './products-list/products-list.component';
 
@@ -26,6 +27,7 @@ const routes: Routes = [
   { path: 'sunglasses', component: ProductsListComponent },
   { path: 'groceries', component: ProductsListComponent },
   { path: 'product/:id', component: ProductComponent },
+  { path: '**', component: Error404Component },
 ];
 
 @NgModule({
