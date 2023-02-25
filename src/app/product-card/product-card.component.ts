4862fa36670a3a32521be1c7a912e675ef9d8531
Patrice-H <webmestre64@gmail.com>
2023-02-25 10:56:08 +0100

    Add click event to show product details

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,14 +1,19 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Product } from '../models/product.model';
 
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
-  styleUrls: ['./product-card.component.scss']
+  styleUrls: ['./product-card.component.scss'],
 })
 export class ProductCardComponent implements OnInit {
   @Input() product!: Product;
-  ngOnInit(): void {
-    
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {}
+  showProductDetails(id: number): void {
+    this.router.navigate([`/product/${id}`]);
   }
 }
