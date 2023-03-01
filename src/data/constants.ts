e4876f80668b927ee7cf3afb590c964b47127a37
Patrice-H <webmestre64@gmail.com>
2023-03-01 22:32:57 +0100

    Add data constant

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -30,3 +30,43 @@ export const CATEGORIES: string[] = [
   'sunglasses',
   'groceries',
 ];
+export const RUBRIC_CATEGORIES: any[] = [
+  {
+    name: 'high-tech',
+    categories: ['smartphones', 'laptops'],
+  },
+  {
+    name: 'home',
+    categories: ['home-decoration', 'lighting', 'furniture'],
+  },
+  {
+    name: 'cosmetic',
+    categories: ['fragances', 'skincare'],
+  },
+  {
+    name: 'auto-moto',
+    categories: ['automotive', 'motorcycle'],
+  },
+  {
+    name: 'women',
+    categories: ['tops', 'womens-dresses', 'womens-shoes'],
+  },
+  {
+    name: 'men',
+    categories: ['mens-shirts', 'mens-shoes'],
+  },
+  {
+    name: 'accessory',
+    categories: [
+      'mens-watches',
+      'womens-watches',
+      'womens-bags',
+      'womens-jewellery',
+      'sunglasses',
+    ],
+  },
+  {
+    name: 'others',
+    categories: ['groceries'],
+  },
+];
