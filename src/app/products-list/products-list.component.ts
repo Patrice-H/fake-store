b59660df0e93a2991a299c190ba2b2a8288045e0
Patrice-H <webmestre64@gmail.com>
2023-03-01 22:33:26 +0100

    Update products list component

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -1,31 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Product } from '../models/product.model';
 import { ProductsService } from '../services/products.service';
-import { RUBRICS } from 'src/data/constants';
+import { RUBRIC_CATEGORIES } from 'src/data/constants';
+import { Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
   styleUrls: ['./products-list.component.scss'],
 })
-export class ProductsListComponent implements OnInit {
+export class ProductsListComponent implements OnInit, OnDestroy {
   productList!: Product[];
+  productList$!: Observable<any>;
+  private destroy$!: Subject<boolean>;
 
   constructor(private productsService: ProductsService) {}
 
   ngOnInit(): void {
-    this.getProducts();
+    this.destroy$ = new Subject<boolean>();
+    this.getProductsList();
   }
 
-  getProducts(): void {
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.productList = [];
+  }
+
+  getProductsList(): void {
     const selectedMenu = window.location.href.split('/')[3];
+    let rubrics: string[] = [];
+    RUBRIC_CATEGORIES.forEach((rubric) => {
+      rubrics.push(rubric.name);
+    });
     if (selectedMenu === '') {
-      this.productList = this.productsService.getAllProducts();
-    } else if (RUBRICS.includes(selectedMenu)) {
-      this.productList = this.productsService.getProductsByRubric(selectedMenu);
+      this.productList$ = this.productsService.getAllProducts();
+      this.productList$.pipe(takeUntil(this.destroy$)).subscribe((value) => {
+        this.productList = value.products;
+      });
+    } else if (rubrics.includes(selectedMenu)) {
+      let rubric: any | undefined = RUBRIC_CATEGORIES.find(
+        (rubric) => rubric.name === selectedMenu
+      );
+      this.productList$ = this.productsService.getAllProducts();
+      this.productList$.pipe(takeUntil(this.destroy$)).subscribe((value) => {
+        this.productList = this.productsService.filterByRubric(
+          value.products,
+          rubric?.categories
+        );
+      });
     } else {
-      this.productList =
+      this.productList$ =
         this.productsService.getProductsByCategory(selectedMenu);
+      this.productList$.pipe(takeUntil(this.destroy$)).subscribe((value) => {
+        this.productList = value.products;
+      });
     }
   }
 }
