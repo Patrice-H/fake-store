7553e19d3bc10e24e23fab86ca4ac851df30fb2a
Patrice-H <webmestre64@gmail.com>
2023-03-02 13:51:52 +0100

    Remove unused elements

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -7,64 +7,6 @@ import { Product } from '../models/product.model';
   providedIn: 'root',
 })
 export class ProductsService {
-  products: Product[] = [
-    new Product(
-      1,
-      'iPhone 9',
-      'An apple mobile which is nothing like apple',
-      549,
-      12.96,
-      4.69,
-      94,
-      'Apple',
-      'smartphones',
-      'https://i.dummyjson.com/data/products/1/thumbnail.jpg',
-      [
-        'https://i.dummyjson.com/data/products/1/1.jpg',
-        'https://i.dummyjson.com/data/products/1/2.jpg',
-        'https://i.dummyjson.com/data/products/1/3.jpg',
-        'https://i.dummyjson.com/data/products/1/4.jpg',
-        'https://i.dummyjson.com/data/products/1/thumbnail.jpg',
-      ]
-    ),
-    new Product(
-      6,
-      'MacBook Pro',
-      'MacBook Pro 2021 with mini-LED display may launch between September, November',
-      1749,
-      11.02,
-      4.57,
-      83,
-      'Apple',
-      'laptops',
-      'https://i.dummyjson.com/data/products/6/thumbnail.png',
-      [
-        'https://i.dummyjson.com/data/products/6/1.png',
-        'https://i.dummyjson.com/data/products/6/2.jpg',
-        'https://i.dummyjson.com/data/products/6/3.png',
-        'https://i.dummyjson.com/data/products/6/4.jpg',
-      ]
-    ),
-    new Product(
-      11,
-      'perfume Oil',
-      'Mega Discount, Impression of Acqua Di Gio by GiorgioArmani concentrated attar perfume Oil',
-      13,
-      8.4,
-      4.26,
-      65,
-      'Impression of Acqua Di Gio',
-      'fragrances',
-      'https://i.dummyjson.com/data/products/11/thumbnail.jpg',
-      [
-        'https://i.dummyjson.com/data/products/11/1.jpg',
-        'https://i.dummyjson.com/data/products/11/2.jpg',
-        'https://i.dummyjson.com/data/products/11/3.jpg',
-        'https://i.dummyjson.com/data/products/11/thumbnail.jpg',
-      ]
-    ),
-  ];
-
   constructor(private http: HttpClient) {}
 
   getAllProducts(): Observable<any> {
@@ -79,7 +21,7 @@ export class ProductsService {
     );
   }
 
-  getProduct(id: number): Observable<any> {
+  getProductById(id: number): Observable<any> {
     return this.http.get<any>(`https://dummyjson.com/products/${id}`);
   }
 
@@ -94,14 +36,5 @@ export class ProductsService {
     });
 
     return productsList;
-  }
-
-  getProductById(id: number): Product {
-    const product = this.products.find((product) => product.id === id);
-    if (product) {
-      return product;
-    } else {
-      throw new Error('Product not found');
-    }
   }
 }
