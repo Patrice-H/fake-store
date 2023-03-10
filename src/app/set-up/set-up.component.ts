1301d9bf358a385b3a9858ef02a01ee7a6b70c83
Patrice-H <webmestre64@gmail.com>
2023-03-10 11:24:31 +0100

    Define brands list of products list

--- src/app/set-up/set-up.component.ts
+++ src/app/set-up/set-up.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from '../models/product.model';
 
 @Component({
   selector: 'app-set-up',
   templateUrl: './set-up.component.html',
-  styleUrls: ['./set-up.component.scss']
+  styleUrls: ['./set-up.component.scss'],
 })
-export class SetUpComponent {
+export class SetUpComponent implements OnInit {
+  @Input() productList!: Product[];
+  brandsList!: string[];
 
+  ngOnInit() {
+    this.brandsList = this.getProductsBrands();
+  }
+
+  getProductsBrands() {
+    let brands: string[] = [];
+    this.productList.forEach((product) => {
+      if (!brands.includes(product.brand)) {
+        brands.push(product.brand);
+      }
+    });
+
+    return brands;
+  }
 }
