d2ae2fb0a9fbd965e2be03744648d6b5de1052fa
Patrice-H <webmestre64@gmail.com>
2023-03-14 16:49:14 +0100

    Implements minimum and maximum prices form

--- src/app/filtering/filtering.component.ts
+++ src/app/filtering/filtering.component.ts
@@ -11,8 +11,9 @@ export class FilteringComponent implements OnInit {
   @Input() brandsList!: string[];
   @Input() minPrice!: number;
   @Input() maxPrice!: number;
-  brandsFilter!: string[];
   brandsForm!: FormGroup;
+  pricesForm!: FormGroup;
+  brandsFilter!: string[];
   minPriceFilter!: number;
   maxPriceFilter!: number;
 
@@ -20,6 +21,7 @@ export class FilteringComponent implements OnInit {
 
   ngOnInit() {
     this.brandsForm = this.formBuilder.group(this.initBrandsCheckbox());
+    this.pricesForm = this.formBuilder.group(this.initPricesInput());
     this.minPriceFilter = this.minPrice;
     this.maxPriceFilter = this.maxPrice;
   }
@@ -31,6 +33,15 @@ export class FilteringComponent implements OnInit {
     });
 
     return brandsCheckbox;
+  }
+
+  initPricesInput() {
+    const pricesInput: any = {
+      min_price: this.minPrice,
+      max_price: this.maxPrice,
+    };
+
+    return pricesInput;
   }
 
   formatBrandName(brand: string) {
@@ -47,6 +58,15 @@ export class FilteringComponent implements OnInit {
     this.brandsFilter = brandsFilter;
   }
 
+  setMinPriceFilter() {
+    if (this.pricesForm.value.min_price > this.minPrice) {
+      this.minPriceFilter = this.pricesForm.value.min_price;
+    }
+    if (this.pricesForm.value.max_price < this.maxPrice) {
+      this.maxPriceFilter = this.pricesForm.value.max_price;
+    }
+  }
+
   encodeBrandsFilter(): string {
     let filters = '';
     this.brandsFilter.forEach((filter) => {
@@ -59,14 +79,33 @@ export class FilteringComponent implements OnInit {
     return filters;
   }
 
-  filterByBrands() {
+  applyFilters() {
     this.setBrandsFilter();
-    let brandsFilter = this.encodeBrandsFilter();
+    this.setMinPriceFilter();
     const url = this.router.url.split('?')[0];
-    if (this.brandsFilter.length > 0) {
-      this.router.navigate([url], { queryParams: { brands: brandsFilter } });
+    if (
+      this.brandsFilter.length > 0 ||
+      this.minPriceFilter > this.minPrice ||
+      this.maxPriceFilter < this.maxPrice
+    ) {
+      this.router.navigate([url], { queryParams: this.getParams() });
     } else {
       this.router.navigate([url]);
     }
+  }
+
+  getParams() {
+    const params: any = {};
+    if (this.brandsFilter.length > 0) {
+      params.brands = this.encodeBrandsFilter();
+    }
+    if (this.minPriceFilter > this.minPrice) {
+      params.min_price = this.minPriceFilter;
+    }
+    if (this.maxPriceFilter < this.maxPrice) {
+      params.max_price = this.maxPriceFilter;
+    }
+
+    return params;
   }
 }
