9ccc8c6cab4f4c386d0428d10c0ce2be71db6ab8
Patrice-H <webmestre64@gmail.com>
2023-04-26 17:40:52 +0200

    Manage filtering form group from this parent component

--- src/app/set-up/set-up.component.ts
+++ src/app/set-up/set-up.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { Product } from '../models/product.model';
 import { ProductsService } from '../services/products.service';
 import { filteringService } from '../services/filtering.service';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-set-up',
@@ -13,6 +14,7 @@ export class SetUpComponent implements OnInit {
   @Input() productList!: Product[];
   brandsList!: string[];
   brandsFiltersList!: string[];
+  brandsForm!: FormGroup;
   minPrice!: number;
   maxPrice!: number;
   params: any = {};
@@ -20,13 +22,18 @@ export class SetUpComponent implements OnInit {
   constructor(
     private productsService: ProductsService,
     private filteringService: filteringService,
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder
   ) {}
 
   ngOnInit() {
     this.brandsList = this.productsService.getProductsBrands(this.productList);
     this.minPrice = this.productsService.getMinPrice(this.productList);
     this.maxPrice = this.productsService.getMaxPrice(this.productList);
+    this.brandsForm = this.formBuilder.group(
+      this.filteringService.initBrandsCheckbox(this.brandsList)
+    );
+    this.brandsFiltersList = [];
   }
 
   sortProducts(sortKey: string) {
@@ -49,6 +56,106 @@ export class SetUpComponent implements OnInit {
       });
     }
     this.brandsFiltersList = filterList;
+  }
+
+  setParams() {
+    let baseUrl = this.router.url.split('?')[0];
+    let parameters = this.router.url.split('?')[1];
+    let brands = '';
+    let min_price, max_price, sort;
+    if (parameters !== undefined && parameters.includes('min_price')) {
+      min_price = parameters.split('min_price=')[1].split('&')[0];
+    }
+    if (parameters !== undefined && parameters.includes('max_price')) {
+      max_price = parameters.split('max_price=')[1].split('&')[0];
+    }
+    if (parameters !== undefined && parameters.includes('sort')) {
+      sort = parameters.split('sort=')[1].split('&')[0];
+    }
+
+    if (this.brandsFiltersList.length > 0) {
+      this.brandsList.forEach((brand) => {
+        if (
+          this.brandsFiltersList.includes(
+            this.filteringService.formatBrandName(brand)
+          )
+        ) {
+          if (brands !== '') {
+            brands += '-';
+          }
+          brands += this.filteringService.formatBrandName(brand);
+        }
+      });
+    }
+
+    if (brands !== '') {
+      this.params.brands = brands;
+    } else {
+      delete this.params.brands;
+    }
+    if (min_price !== undefined && Number(min_price) > this.minPrice) {
+      this.params.min_price = min_price;
+    } else {
+      delete this.params.min_price;
+    }
+    if (max_price !== undefined && Number(max_price) < this.maxPrice) {
+      this.params.max_price = max_price;
+    } else {
+      delete this.params.max_price;
+    }
+    if (sort !== undefined) {
+      this.params.sort = sort;
+    } else {
+      delete this.params.sort;
+    }
+    if (
+      this.params.brands ||
+      this.params.min_price ||
+      this.params.max_price ||
+      this.params.sort
+    ) {
+      this.router.navigate([baseUrl], { queryParams: this.params });
+    } else {
+      this.router.navigate([baseUrl]);
+    }
+  }
+
+  setBrandsForm() {
+    this.brandsList.forEach((brand) => {
+      if (
+        this.brandsFiltersList.includes(
+          this.filteringService.formatBrandName(brand)
+        )
+      ) {
+        this.brandsForm.controls[
+          this.filteringService.formatBrandName(brand)
+        ].setValue(true);
+      } else {
+        this.brandsForm.controls[
+          this.filteringService.formatBrandName(brand)
+        ].setValue(false);
+      }
+    });
+  }
+
+  toggleFilter(filter: string) {
+    if (this.brandsFiltersList.includes(filter)) {
+      this.brandsFiltersList = this.brandsFiltersList.filter(
+        (brand) => brand !== filter
+      );
+    } else {
+      this.brandsFiltersList.push(filter);
+    }
+    this.setBrandsForm();
+    this.setParams();
+  }
+
+  removeFilter(filter: string) {
+    this.brandsFiltersList = this.brandsFiltersList.filter(
+      (brand) => brand !== filter
+    );
+    this.setBrandsForm();
+    this.setParams();
   }
 
   filterProducts(filterKey: any) {
