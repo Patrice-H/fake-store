a8eeee74a9a100e340aa30f49508d97871f3d1ce
Patrice-H <webmestre64@gmail.com>
2023-04-26 17:32:40 +0200

    Format file

--- src/app/services/filtering.service.ts
+++ src/app/services/filtering.service.ts
@@ -2,50 +2,50 @@ import { Injectable } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
 export class filteringService {
-    initBrandsCheckbox(brandsList: string[]) {
-        const brandsCheckbox: any = {};
-        brandsList.forEach((brand) => {
-          brandsCheckbox[this.formatBrandName(brand)] = false;
-        });
-    
-        return brandsCheckbox;
-    }
+  initBrandsCheckbox(brandsList: string[]) {
+    const brandsCheckbox: any = {};
+    brandsList.forEach((brand) => {
+      brandsCheckbox[this.formatBrandName(brand)] = false;
+    });
 
-    initPricesInput(minPrice: number, maxPrice: number) {
-        const pricesInput: any = {
-          min_price: minPrice,
-          max_price: maxPrice,
-        };
-    
-        return pricesInput;
-    }
+    return brandsCheckbox;
+  }
 
-    setBrandsFilter(brandsList: string[], brandsForm: FormGroup) {
-        let brandsFilter: string[] = [];
-        brandsList.forEach((brand) => {
-          if (brandsForm.value[this.formatBrandName(brand)]) {
-            brandsFilter.push(this.formatBrandName(brand));
-          }
-        });
-        return brandsFilter;
-    }
+  initPricesInput(minPrice: number, maxPrice: number) {
+    const pricesInput: any = {
+      min_price: minPrice,
+      max_price: maxPrice,
+    };
 
-    encodeBrandsFilter(brandsFilter: string[]): string {
-        let filters = '';
-        brandsFilter.forEach((filter) => {
-          if (filters !== '') {
-            filters += '-';
-          }
-          filters += filter;
-        });
-    
-        return filters;
-    }
+    return pricesInput;
+  }
 
-    formatBrandName(brand: string) {
-        return brand.toLowerCase().split(' ').join('');
+  setBrandsFilter(brandsList: string[], brandsForm: FormGroup) {
+    let brandsFilter: string[] = [];
+    brandsList.forEach((brand) => {
+      if (brandsForm.value[this.formatBrandName(brand)]) {
+        brandsFilter.push(this.formatBrandName(brand));
       }
+    });
+    return brandsFilter;
+  }
+
+  encodeBrandsFilter(brandsFilter: string[]): string {
+    let filters = '';
+    brandsFilter.forEach((filter) => {
+      if (filters !== '') {
+        filters += '-';
+      }
+      filters += filter;
+    });
+
+    return filters;
+  }
+
+  formatBrandName(brand: string) {
+    return brand.toLowerCase().split(' ').join('');
+  }
 }
