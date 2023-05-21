80840132b458cb371e88598fb7d1011ebdf0afea
Patrice-H <webmestre64@gmail.com>
2023-05-21 21:42:06 +0200

    Implements search bar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  search!: FormControl;
+  @Output() searchEvent = new EventEmitter<string>();
+
   constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.search = new FormControl('');
+  }
 
   goToCartPage() {
     this.router.navigate(['cart']);
+  }
+
+  setSearch(): void {
+    this.searchEvent.emit(this.search.value);
   }
 }
