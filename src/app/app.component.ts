000d58d9fdf9e1f297afe97716e1926b8ab86027
Patrice-H <webmestre64@gmail.com>
2023-12-25 14:35:35 +0100

    Adds observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProductsService } from './services/products.service';
+import { Router } from '@angular/router';
+import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  productsSearched!: Product[];
+  allProducts$!: Observable<any>;
+  allProducts!: Product[];
+
+  constructor(
+    private productsService: ProductsService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.allProducts$ = this.productsService.getAllProducts();
+    this.allProducts$.subscribe((value) => {
+      this.allProducts = Array.from(value.products);
+    });
+    this.productsSearched = this.allProducts;
+  }
+
+  searchProducts(search: string) {
+    this.productsSearched = this.productsService.researchProducts(
+      this.allProducts,
+      search
+    );
+
+    console.log(this.productsSearched);
+    this.router
+      .navigateByUrl('/', { skipLocationChange: true })
+      .then(() => this.router.navigate([]));
   }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product.model';
+import { ProductsService } from '../services/products.service';
+import { RUBRIC_CATEGORIES } from 'src/data/constants';
 
 @Component({
   selector: 'app-header',
@@ -10,8 +14,14 @@ import { FormControl } from '@angular/forms';
 export class HeaderComponent implements OnInit {
   search!: FormControl;
   @Output() searchEvent = new EventEmitter<string>();
+  productList$!: Observable<any>;
+  productList!: Product[];
+  rubricsList!: string[];
 
-  constructor(private router: Router) {}
+  constructor(
+    private productsService: ProductsService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.search = new FormControl('');
@@ -23,5 +33,63 @@ export class HeaderComponent implements OnInit {
 
   setSearch(): void {
     this.searchEvent.emit(this.search.value);
+  }
+
+  setRubrics(): string[] {
+    let rubrics: string[] = [];
+    RUBRIC_CATEGORIES.forEach((rubric) => {
+      rubrics.push(rubric.name);
+    });
+
+    return rubrics;
+  }
+  /*
+  setProductsDisplayed() {
+    let products: Product[] = [];
+    if (this.productList === undefined) {
+      return;
+    }
+    let brands: string[] = [];
+    if (this.brandsFilter !== undefined) {
+      brands = this.brandsFilter.split('-');
+    }
+    if (isNaN(this.minPrice)) {
+      this.minPrice = this.productsService.getMinPrice(this.productList);
+    }
+    if (isNaN(this.maxPrice)) {
+      this.maxPrice = this.productsService.getMaxPrice(this.productList);
+    }
+    products = this.productsService.filterByBrands(this.productList, brands);
+    products = this.productsService.filterByPrice(
+      products,
+      this.minPrice,
+      this.maxPrice
+    );
+    products = this.productsService.sortProducts(products, this.sortKey);
+    this.productsDisplayed = products;
+  }
+*/
+  getProductsList(): void {
+    const selectedMenu = this.router.url.split('/')[1].split('?')[0];
+    if (selectedMenu === '' || this.rubricsList.includes(selectedMenu)) {
+      this.productList$ = this.productsService.getAllProducts();
+    } else {
+      this.productList$ =
+        this.productsService.getProductsByCategory(selectedMenu);
+    }
+    this.productList$.subscribe((value) => {
+      if (this.rubricsList.includes(selectedMenu)) {
+        let rubric: any | undefined = RUBRIC_CATEGORIES.find(
+          (rubric) => rubric.name === selectedMenu
+        );
+        this.productList = this.productsService.filterByRubric(
+          value.products,
+          rubric?.categories
+        );
+      } else {
+        this.productList = value.products;
+      }
+      //this.setProductsDisplayed();
+    });
   }
 }
